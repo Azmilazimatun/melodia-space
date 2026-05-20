c3c75b282348a7ccc8cda450ef7fbea0a689b1b4
NamaKamu <azmilazimatun@gmail.com>
2026-05-20 13:29:10 +0700

    fix railway deploy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -25,6 +26,10 @@ CREATE TABLE IF NOT EXISTS booking (
 """)
 
 conn.commit()
+
+@app.route('/')
+def home():
+    return "Melodia Space Backend Running 🚀"
 
 @app.route('/api/booking', methods=['GET'])
 def get_booking():
@@ -129,4 +134,5 @@ def hapus_booking(id):
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
